cbdf5a3573e8c7b4285fe3b44ad65b2130c289cd
elsidora <ella-sidorova@mail.ru>
2019-07-23 13:50:22 +0300

    lesson24

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const elem = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder='search' />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>      
+);
+console.log(elem);
+ReactDOM.render(elem, document.querySelector('#root'));
